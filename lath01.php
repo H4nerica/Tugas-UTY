f5be84a310852a36e2b6c35ae1cd4dd49edb7c28
Hane <yunominath4me@gmail.com>
2024-11-30 07:33:07 +0700

    Add files via upload

---
+++ lath01.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Kode PHP dalam HTML</title>    
+</head>
+
+<body>
+    PRAKTIK Komunikasi data - Teknik Komputer UTY
+<br>
+NAMA    :   MUHAMMAD NATHAN JAYANI
+NPM     :   5231011045
+<br>
+<br>
+<?php
+printf("Tanggal Hari Iini: %s ", Date("d F Y"));
+?>
+</body>
+</html>
