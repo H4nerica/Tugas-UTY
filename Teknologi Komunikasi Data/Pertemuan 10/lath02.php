eb2e5d5fb9fc42eb7b59e1e20d812ccdc483d478
Hane <yunominath4me@gmail.com>
2024-12-03 20:15:47 +0700

    Create lath02.php

---
+++ Teknologi Komunikasi Data/Pertemuan 10/lath02.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Pengulangan dengan for</title>
+</head>
+<body>
+<?php
+    for($i = 1; $i <= 7; $i = $i + 1) {
+        print("<FONT SIZE=$i> $i </FONT><BR>");
+    }
+    print("Yak!");
+?>
+</body>
+</html>
